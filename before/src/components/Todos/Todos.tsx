656e968ee3c4597806aa7f1802cfa4481ef7f3e7
kuma-coffee <daniel099d@gmail.com>
2024-10-30 16:08:42 +0700

    fix env and minor fix todos

--- before/src/components/Todos/Todos.tsx
+++ before/src/components/Todos/Todos.tsx
@@ -1,7 +1,6 @@
 import { useGetTodos } from '@/hooks/todos/useGetTodos';
 import React from 'react';
-import TrashIcon from '@/assets/svg/icon-trash.svg';
-import styles from './Todos.module.scss';
+// import styles from './Todos.module.scss';
 
 export const Todos: React.FC = () => {
   const { todos, isFetching, queryKey } = useGetTodos({
@@ -10,34 +9,5 @@ export const Todos: React.FC = () => {
 
   console.log(todos, isFetching, queryKey);
 
-  return (
-    <div className={styles.todoContainer}>
-      {/* <form onSubmit={onSubmitForm}>
-        <input
-          type='text'
-          value={newTodoText}
-          className={styles.newTodoInput}
-          onChange={(e) => setNewTodoText(e.target.value)}
-        />
-      </form>
-      <ul className={styles.todos}>
-        {isFetching && <li>Data is Fetching...</li>}
-        {todos.map((todo) => (
-          <li key={todo.id}>
-            <input
-              type='text'
-              defaultValue={todo.title}
-              data-id={todo.id}
-              onKeyDown={handleUpdateTodo}
-            />
-            <TrashIcon
-              className={styles.deleteIcon}
-              data-id={todo.id}
-              onClick={handleDeleteTodo}
-            />
-          </li>
-        ))}
-      </ul> */}
-    </div>
-  );
+  return <h1>Todos</h1>;
 };
