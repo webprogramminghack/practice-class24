1ea0017fb888a6c893cb7b7ae010a15eb8071556
kuma-coffee <daniel099d@gmail.com>
2024-11-04 12:43:59 +0700

    add useSearchParams for completed

--- after/src/components/Todos/Todos.tsx
+++ after/src/components/Todos/Todos.tsx
@@ -1,15 +1,43 @@
 import { useGetTodos } from '@/hooks/todos/useGetTodos';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 // import styles from './Todos.module.scss';
 
 export const Todos: React.FC = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const completedParam = searchParams.get('completed');
+
+  const completed =
+    completedParam === 'true'
+      ? true
+      : completedParam === 'false'
+      ? false
+      : undefined;
+
   const { todos } = useGetTodos({
     order: 'desc',
+    completed,
   });
+
+  const handleCheckbox = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchParams({ completed: e.target.checked.toString() });
+  };
+
+  const handleReset = () => {
+    setSearchParams({});
+  };
 
   return (
     <>
+      <label htmlFor='completed'>Is Completed?</label>
+      <input
+        type='checkbox'
+        name='completed'
+        id='completed'
+        checked={completed}
+        onChange={handleCheckbox}
+      />
+      <button onClick={handleReset}>Reset</button>
       {todos.map((todo) => (
         <div key={todo.id}>
           <Link to={todo.id}>{todo.title}</Link>
